e7ca2bd488c1bb6a350e7d75665d4cde2b1b2cb8
Ridho894 <77140390+Ridho894@users.noreply.github.com>
2021-09-06 09:25:31 +0700

    add data success

--- app/add.php
+++ app/add.php
@@ -6,6 +6,19 @@ if (isset($_POST['title'])) {
 
     if (empty($title)) {
         header("location: ../index.php?mess=error");
+    } else {
+        $stmt = $conn->prepare("INSERT INTO todos(title) VALUE(?)");
+        $res = $stmt->execute([$title]);
+
+        if ($res) {
+            header("location: ../index.php?mess=success");
+        } else {
+            header("location: ../index.php");
+        }
+        $conn = null;
+        exit();
     }
+} else {
+    header("location: ../index.php?mess=error");
 }
 ?>
--- index.php
+++ index.php
@@ -16,7 +16,7 @@
         <div class="add-section">
             <form action="app/add.php" method="POST" autocomplete="off">
                 <?php if (isset($_GET['mess']) && $_GET['mess'] == 'error') { ?>
-                    <input type="text" name="title" placeholder="This field is required" />
+                    <input type="text" name="title" placeholder="This field is required" style="border-color: #ff6666;" />
                     <button type="submit">Add &nbsp; <span>&#43;</span></button>
                 <?php } else { ?>
                     <input type="text" name="title" placeholder="What do you need to do?" />
