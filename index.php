110873e6e680edeaec78ba371cb9a3ab46da4a1e
Ridho894 <77140390+Ridho894@users.noreply.github.com>
2021-09-06 09:10:03 +0700

    data sudah bisa tampil

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    require 'db_conn.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,18 +14,38 @@
 <body>
     <div class="main-section">
         <div class="add-section">
-            <form action="">
+            <form action="" method="POST" autocomplete="off">
                 <input type="text" name="title" placeholder="This field is required" />
                 <button type="submit">Add &nbsp; <span>&#43;</span></button>
             </form>
         </div>
+        <?php
+            $todos = $conn->query("SELECT * FROM todos ORDER BY id DESC")
+        ?>
         <div class="show-todo-section">
+            <?php if ($todos->rowCount() <= 0) { ?>
+                <div class="todo-item">
+                    <div class="empty">
+                        <img src="https://image.freepik.com/free-vector/no-data-concept-illustration_114360-536.jpg" width="100%" />
+                    </div>
+                </div>
+            <?php } ?>
+
+            <?php while($todo = $todos->fetch(PDO::FETCH_ASSOC)) { ?>
             <div class="todo-item">
-                <input type="checkbox">
-                <h2>This is check</h2>
+                <span id="<?php echo $todo['id']; ?>" class="remove-to-do">x</span>
+                <?php if ($todo['checked']) { ?>
+                    <input type="checkbox" class="check-box" checked />
+                    <h2 class="checked"><?php echo $todo['title']; ?></h2>
+                <?php } else { ?>
+                    <input type="checkbox" class="check-box" />
+                    <h2><?php echo $todo['title']; ?></h2>
+                <?php } ?>
+
                 <br>
-                <small>created: 3/3/2021</small>
+                <small>Created: <?php echo $todo['date_time'] ?></small>
             </div>
+            <?php } ?>
         </div>
     </div>
 </body>
